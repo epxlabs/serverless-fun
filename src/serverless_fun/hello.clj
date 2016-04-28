4c0221fafa288fc5ccb72fd3cab1bd18f579938c
Prachetas Prabhu <prachetas.github@gmail.com>
2016-04-28 18:56:39 -0400

    First commit - nonworking

---
+++ src/serverless_fun/hello.clj
@@ -0,0 +1,8 @@
+(ns serverless-fun.hello
+  (:gen-class
+   :methods [^:static [handler [String] String]]))
+
+(defn -handler
+  "Lambda main"
+  [s]
+  (str  "Hello " s "!"))
---
+++ test/serverless_fun/core_test.clj
@@ -0,0 +1,7 @@
+(ns serverless-fun.core-test
+  (:require [clojure.test :refer :all]
+            [serverless-fun.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
